f4cb15995d933ace1695bbc730fe7f58edb9767f
ri-lab <callumolvedinstruments.com>
2019-03-28 11:48:12 -0600

    updated simpledaq to use ni nidaqmx module

--- daq.py
+++ daq.py
@@ -1,6 +1,8 @@
-from nidaqmx import AnalogInputTask
+# from nidaqmx import AnalogInputTask
+import nidaqmx
 
-class SimpleDaq(object):
+# uses old libnidaqmx library
+class SimpleDaq_old(object):
     def __init__(self, channel, rate, n,maxv=3.):
         self.n = n
         
@@ -21,6 +23,41 @@ class SimpleDaq(object):
         data = self.task.read(self.n, fill_mode='group_by_channel')
         self.task.stop()
         return data
+
+class SimpleDaq(object):
+    def __init__(self, channel, rate, n,maxv=3., terminal='diff',):
+        self.n = n
+        
+        self.task = nidaqmx.Task()
+
+        configtable = {
+            'default': nidaqmx.constants.TerminalConfiguration.DEFAULT,
+            'diff': nidaqmx.constants.TerminalConfiguration.DIFFERENTIAL,
+        }
+        
+
+        if isinstance(channel, str):
+            channel = [channel]
+        for c in channel:
+            self.task.ai_channels.add_ai_voltage_chan(
+                c, terminal_config=configtable[terminal],
+                min_val=-maxv, max_val=maxv)
+
+        self.task.timing.cfg_samp_clk_timing(
+            rate=rate, 
+            sample_mode=nidaqmx.constants.AcquisitionType.FINITE, 
+            samps_per_chan=n)
+
+    def read(self, n=None):
+        if n is None:
+            n = self.n
+            
+        self.task.start()
+        data = self.task.read(self.n)
+        self.task.stop()
+        return data
+
+
 
 
 if __name__ == "__main__":
